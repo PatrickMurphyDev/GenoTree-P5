9ba29fcfb84ce371a696054067fdf648b98451ff
Patrick Murphy <patrickmurphydev@gmail.com>
2023-12-16 23:22:54 -0800

    refactor subject entity draw sub functions

--- js/Entity/Subject.Entity.js
+++ js/Entity/Subject.Entity.js
@@ -18,6 +18,10 @@ class SubjectEntity extends Entity {
                             createVector(this.location.x-this.sizePx*linkCoordOffset,this.location.y)];
     }
 
+    /*
+        input subject entity
+        output nearest linkpoint on this object to inobj
+    */
     getNearestLinkPoint(SubIn){
         var lowestDist = 9999;
         var lowestObj;
@@ -36,60 +40,55 @@ class SubjectEntity extends Entity {
         super.update();
     }
 
-    display(){
-        var sizePx = this.sizePx;
-
-        // Title Text label
-        fill(this.fillColor);
-        if(this.strokeWeight>0){
-            stroke(this.strokeColor);
-            this.strokeWeight = this.strokeWeight;
-        }else{
-            noStroke();
-        }
-
+    drawLabel(){
         if(this.elementList.includes("Label")){
             var tmpTxt = this.label;
-            text(tmpTxt,this.location.x-textWidth(tmpTxt)/2,this.location.y+sizePx);    
+            text(tmpTxt,this.location.x-textWidth(tmpTxt)/2,this.location.y+this.sizePx);    
         }
 
+    }
+
+    drawShape(){
         noFill();
         stroke(this.strokeColor);
         strokeWeight(this.strokeWeight);
 
         // Gender
         if(this.elementList.includes("GenderFemale")){
-            circle(this.location.x,this.location.y, sizePx);
+            circle(this.location.x,this.location.y, this.sizePx);
         }
         if(this.elementList.includes("GenderMale" )){
-            rect(this.location.x,this.location.y, sizePx);
-        }
-
-        // Dead / Alive
-        if(!this.isAlive){
-            line(this.location.x-sizePx/2,this.location.y-sizePx/2,this.location.x+sizePx/2,this.location.y+sizePx/2,);
-            line(this.location.x+sizePx/2,this.location.y-sizePx/2,this.location.x-sizePx/2,this.location.y+sizePx/2,);
+            rect(this.location.x,this.location.y, this.sizePx);
         }
         
         // Miscarrage
         fill(this.fillColor);
         if(this.elementList.includes("Miscarrage")){
-            circle(this.location.x,this.location.y, sizePx*.2);
+            circle(this.location.x,this.location.y, this.sizePx*.2);
         }
 
         // diamond pet
         if(this.elementList.includes("Pet")){
-            push();
+            push(); // push transformation matrix
             noFill();
             translate(this.location.x,this.location.y);
             rotate(45);
             rect(0,0,sizePx*.75);
-            pop();
+            pop(); // restore transformation matrix
         }
-
+        
         // question mark unknown gender
-        // image or icon
+    }
 
+    // Dead / Alive
+    drawDeceasedStatus(){
+        if(!this.isAlive){
+            line(this.location.x-this.sizePx/2,this.location.y-this.sizePx/2,this.location.x+this.sizePx/2,this.location.y+this.sizePx/2,);
+            line(this.location.x+this.sizePx/2,this.location.y-this.sizePx/2,this.location.x-this.sizePx/2,this.location.y+this.sizePx/2,);
+        }
+    }
+
+    drawImage(){
         noStroke();        
         strokeWeight(1);
 
@@ -98,11 +97,33 @@ class SubjectEntity extends Entity {
             image(this.image_path,this.location.x,this.location.y);
             imageMode(CORNER);
         }
+    }
 
+    drawLinkPoints(){
         stroke(color(255,0,0));
         rect(this.linkCoords[0].x,this.linkCoords[0].y,2);
         rect(this.linkCoords[1].x,this.linkCoords[1].y,2);
         rect(this.linkCoords[2].x,this.linkCoords[2].y,2);
         rect(this.linkCoords[3].x,this.linkCoords[3].y,2);
+    }
+
+    // set style based on this var state
+    setDrawStyle(){
+        fill(this.fillColor);
+        if(this.strokeWeight>0){
+            stroke(this.strokeColor);
+            this.strokeWeight = this.strokeWeight;
+        }else{
+            noStroke();
+        }
+    }
+
+    display(){
+        this.setDrawStyle();
+        this.drawLabel();
+        this.drawShape();
+        this.drawImage(); // move Image into shape so it can be contained in shape
+        this.drawDeceasedStatus();
+        this.drawLinkPoints();
     }
 }
